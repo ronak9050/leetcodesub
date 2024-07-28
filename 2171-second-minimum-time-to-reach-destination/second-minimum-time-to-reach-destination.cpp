c0cf0a601f1d6e61c3ea73cf1847edacffb9cfa6
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-07-28 12:20:02 +0530

    Time: 426 ms (83.13%) | Memory: 196.6 MB (67.99%) - LeetSync

---
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    int secondMinimum(int n, vector<vector<int>>& edges, int t, int c) {
+        vector<vector<int>> g(n+1);
+        for(auto &e:edges){
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+        vector<int> vis(n+1,0),path(n+1,-1);
+        queue<pair<int,int>> q;
+        q.push({1,0});
+        int last=0,ct=0;
+        while(!q.empty()){
+            int  cur=q.front().first;
+            int time=q.front().second;
+            q.pop();
+            
+            if(cur==n && time>last){
+                last=time;
+                ct++;
+                if(ct==2) return last;
+            }
+            else if(cur==n) continue;
+            if(path[cur]>=time) continue;
+            vis[cur]++;
+            path[cur]=time;
+            for(auto &e:g[cur]){
+                if(vis[e]>=2) continue;
+                int pt;
+                if((time/c)&1) pt=(time/c+1)*c+t;
+                else pt=time+t;
+                q.push({e,pt});
+            }
+        }
+        return -1;
+    }
+};
