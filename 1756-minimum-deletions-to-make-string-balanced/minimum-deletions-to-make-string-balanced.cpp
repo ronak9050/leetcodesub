7e97da7591eefce03bcf7e5177c5d18c16859209
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-07-30 12:34:11 +0530

    Time: 93 ms (55.50%) | Memory: 23.5 MB (80.38%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n=s.size();
-        vector<int> fwd(n,0),bkd(n,0);
-        int ct=0;
+        int ct=0,ans=n,ct2=0;
         for(int i=0; i<n; i++){
-            fwd[i]=ct;
             if(s[i]=='b') ct++;
         }
-        ct=0;
         for(int i=n-1; i>=0; i--){
-            bkd[i]=ct;
-            if(s[i]=='a') ct++;
+            if(s[i]=='b') ct--;
+            ans=min(ans,ct+ct2);
+            if(s[i]=='a') ct2++;
         }
-        int ans=n;
-        for(int i=0; i<n; i++){
-            ans=min(ans,bkd[i]+fwd[i]);
-        }
+        
         return ans;
     }
 };
