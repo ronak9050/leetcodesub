3c226ba388ebadf0557fc1ecc18424fd49a9da79
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-07-31 11:04:38 +0530

    Time: 13 ms (7.46%) | Memory: 20.9 MB (15.97%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int func(int ind,int lvl,int w,vector<vector<int>>&a,vector<vector<int>>&dp){
+    int func(int ind,int rem,int w,int ht,vector<vector<int>>&a,vector<vector<int>>&dp){
         int n=a.size();
-        if(ind>=n) return 0;
-        if(dp[ind][lvl]!=-1) return dp[ind][lvl];
-        int curw=w-a[ind][0],mxht=a[ind][1];
-        int ans=mxht+func(ind+1,lvl+1,w,a,dp);
-        for(int i=ind+1; i<n; i++){
-            if(a[i][0]<=curw){
-                curw-=a[i][0];
-                mxht=max(mxht,a[i][1]);
-                ans=min(ans,mxht+func(i+1,lvl+1,w,a,dp));
-            }
-            else break;
-        }
-        return dp[ind][lvl]=ans;
+        if(ind>=n) return ht;
+        if(dp[ind][rem]!=-1) return dp[ind][rem];
+        int ans=INT_MAX;
+        if(a[ind][0]<=rem) ans=func(ind+1,rem-a[ind][0],w,max(ht,a[ind][1]),a,dp);
+        ans=min(ans,ht+func(ind+1,w-a[ind][0],w,a[ind][1],a,dp));
+        return dp[ind][rem]=ans;
     }
     int minHeightShelves(vector<vector<int>>& a, int w) {
         int n=a.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return func(0,0,w,a,dp);
+        vector<vector<int>> dp(n,vector<int>(w+1,-1));
+        return func(1,w-a[0][0],w,a[0][1],a,dp);
     }
 };
