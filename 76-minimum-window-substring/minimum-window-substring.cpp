cd57f7a20fdf77c84d12be148e41d1bc11f24d5c
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-01 16:03:12 +0530

    Time: 4 ms (94.45%) | Memory: 9.8 MB (83.62%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int st=-1,en=1e9,n=s.size(),i=0,j=0;
+        int st=-1,en=1e9,n=s.size(),i=0,j=0,req=0,cur=0;
         vector<int> tct(60,0),ct(60,0);
         for(auto &e:t) tct[e-'A']++;
+        for(auto &e:tct) if(e) req++;
         while(i<n){
             ct[s[i]-'A']++;
+            if(ct[s[i]-'A']==tct[s[i]-'A']) cur++;
             i++;
+            
             while(j<n && ct[s[j]-'A']>tct[s[j]-'A']){
                 ct[s[j]-'A']--;
                 j++;
             }
-            int f=1;
-            for(int k=0; k<60; k++) if(ct[k]<tct[k]) f=0;
-            if(f){
+
+            if(req==cur){
                 if(en-st>i-j && i>j) st=j,en=i;
             }
+            // if(f){
+                
+            // }
         }
         string ans;
         if(st==-1) return ans;
