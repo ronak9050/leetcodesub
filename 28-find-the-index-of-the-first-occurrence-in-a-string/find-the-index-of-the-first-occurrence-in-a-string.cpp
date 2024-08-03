3d055eb90ec3aeb64c055a1d02b3b08a4255b4aa
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-03 10:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (24.31%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    vector<int> z_function(string s){
+    vector<int> kmp(string s){
         int n=s.size();
-        vector<int> z(n);
-        int l=0,r=0;
+        vector<int> pi(n);
+        
         for(int i=1; i<n; i++){
-            if(i<r) z[i]=min(r-i,z[i-l]);
-            while(i+z[i]<n && s[z[i]]==s[i+z[i]]) z[i]++;
-            if(i+z[i]>r){
-                l=i;
-                r=i+z[i];
+            int j=pi[i-1];
+            while(j!=0 && s[i]!=s[j]){
+                j=pi[j-1];
             }
+            if(s[i]==s[j]) j++;
+            pi[i]=j;
         }
-        return z;
+
+        return pi;
     }
     int strStr(string s, string t) {
         string tmp=t+'#'+s;
-        vector<int> z=z_function(tmp);
+        vector<int> pi=kmp(tmp);
         int n=t.size();
         for(int i=n+1; i<tmp.size(); i++){
-            if(z[i]==n) return i-n-1; 
+            if(pi[i]==n) return i-2*n; 
         }
         return -1;
     }
