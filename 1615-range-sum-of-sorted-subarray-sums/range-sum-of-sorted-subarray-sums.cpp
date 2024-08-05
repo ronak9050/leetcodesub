b82e7bf85c86bcfe9ffc8c2607d13d7a93e65fa9
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-05 13:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (91.77%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,21 +1,39 @@
 #define ll long long
+ll M=1e9+7;
 class Solution {
 public:
-    int rangeSum(vector<int>& a, int n, int left, int right) {
-        priority_queue<ll,vector<ll>,greater<ll>> pq;
+    pair<ll,ll> check(ll mid,ll k,vector<int>&a){
+        ll sum=0,j=0,ct=0,wsum=0,totsum=0,n=a.size();
         for(int i=0; i<n; i++){
-            ll sum=0;
-            for(int j=i; j<n; j++){
-                sum=sum+a[j];
-                pq.push(sum);
+            sum=sum+a[i];
+            wsum=wsum+a[i]*1ll*(i-j+1);
+            while(sum>mid){
+                wsum-=sum;
+                sum-=a[j];
+                j++;
             }
+            ct=ct+(i-j+1);
+            totsum=totsum+wsum;
         }
-        for(int i=0; i<left-1; i++) pq.pop();
-        ll ans=0,M=1e9+7;
-        for(int i=left; i<=right; i++){
-            ans=(ans+pq.top())%M;
-            pq.pop();
+        return {ct,totsum};
+    }
+
+    ll func(int k,vector<int>&a){
+        int n=a.size();
+        ll l=*min_element(a.begin(),a.end());
+        ll h=0;
+        for(auto &e:a) h=h+e;
+        while(l<=h){
+            ll mid=(l+h)/2;
+            pair<ll,ll> cur=check(mid,k,a);
+            if(cur.first>=k) h=mid-1;
+            else l=mid+1;
         }
-        return ans;
+        pair<ll,ll> fin=check(l,k,a);
+        return fin.second-l*(fin.first-k);
+    }
+
+    int rangeSum(vector<int>& a, int n, int left, int right) {
+        return (func(right,a)-func(left-1,a))%M;
     }
 };
