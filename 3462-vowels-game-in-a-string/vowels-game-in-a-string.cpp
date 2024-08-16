a597e375235fa6b65830d98289102afde4c6ee56
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-17 01:21:42 +0530

    Time: 58 ms (11.44%) | Memory: 19.3 MB (6.84%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        map<char,int> mp={{'a',1},{'e',1},{'i',1},{'o',1},{'u',1}};
+        unordered_map <char,int> mp={{'a',1},{'e',1},{'i',1},{'o',1},{'u',1}};
         int ct=0;
-        for(auto &e:s) if(mp[e]) ct++;
-        if(ct) return 1;
+        for(auto &e:s) if(mp[e]) return 1;
         return 0;
     }
 };
