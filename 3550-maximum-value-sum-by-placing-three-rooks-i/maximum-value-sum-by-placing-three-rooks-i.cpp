4f5c2b66e4297298eaea164fdd9b9a5213cb64fa
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-18 03:22:48 +0530

    Time: 1542 ms (15.79%) | Memory: 520.1 MB (5.26%) - LeetSync

--- 3550-maximum-value-sum-by-placing-three-rooks-i/maximum-value-sum-by-placing-three-rooks-i.cpp
+++ 3550-maximum-value-sum-by-placing-three-rooks-i/maximum-value-sum-by-placing-three-rooks-i.cpp
@@ -3,23 +3,29 @@
 class Solution {
 public:
     
-    ll func(int ct,int rem,priority_queue<vector<ll>> pq,vector<int> &row,vector<int> &col){
+    ll func(int ct,int rem,set<vector<ll>> st,vector<int> &row,vector<int> &col){
         if(rem==0) return 0;
-        if(ct==0 || pq.empty()) return -1e15;
+        if(ct==0 || st.empty()) return -1e15;
         
         ll ans=-1e15;
-        auto cur=pq.top();
-        while(pq.size() && (row[pq.top()[1]] || col[pq.top()[2]])){
-            pq.pop();
+        auto it=st.end();
+        it--;
+        while(st.size() && (row[it->at(1)] || col[it->at(2)])){
+            st.erase(it);
+            it=st.end();
+            if(st.size()) it--;
+            else break;
         }
-        if(pq.size()==0) return -1e15;
-        cur=pq.top();
-        pq.pop();
+        if(st.size()==0) return -1e15;
+        it=st.end();
+        it--;
+        auto cur=*it;
+        st.erase(it);
         
-        ans=func(ct-1,rem,pq,row,col);
+        ans=func(ct-1,rem,st,row,col);
         row[cur[1]]=1;
         col[cur[2]]=1;
-        ans=max(ans,cur[0]+func(ct-1,rem-1,pq,row,col));
+        ans=max(ans,cur[0]+func(ct-1,rem-1,st,row,col));
         row[cur[1]]=0;
         col[cur[2]]=0;
         return ans;
@@ -27,14 +33,17 @@ public:
         
     long long maximumValueSum(vector<vector<int>>& a) {
         ll n=a.size(),m=a[0].size();
-        priority_queue<vector<ll>> pq;
+        set<vector<ll>> st;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                pq.push({a[i][j],i,j});
+                st.insert({a[i][j],i,j});
             }
         }
         vector<int> row(n,0),col(m,0);
-        ll ans=func(6,3,pq,row,col);
+        while(st.size()>601){
+            st.erase(st.begin());
+        }
+        ll ans=func(6,3,st,row,col);
         return ans;
     }
 };
