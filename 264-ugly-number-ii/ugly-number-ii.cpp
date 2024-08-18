76686df56c92318cdf75087037dcbbb7f44b51ab
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-18 12:53:44 +0530

    Time: 127 ms (5.32%) | Memory: 32.8 MB (19.29%) - LeetSync

---
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -0,0 +1,21 @@
+#define ll long long
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        priority_queue<ll,vector<ll>,greater<ll>> pq;
+        pq.push(1);
+        map<ll,int> mp;
+        while(n){
+            ll cur=pq.top();
+            pq.pop();
+            if(mp[cur]) continue;
+            n--;
+            mp[cur]=1;
+            if(n==0) return cur;
+            pq.push(cur*2);
+            pq.push(cur*3);
+            pq.push(cur*5);
+        }
+        return 1;
+    }
+};
