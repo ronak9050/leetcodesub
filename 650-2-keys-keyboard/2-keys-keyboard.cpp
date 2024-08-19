1e14461dc8845111639e616572eb1733b077429b
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-19 12:35:30 +0530

    Time: 3 ms (69.17%) | Memory: 7.4 MB (71.95%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int minSteps(int n) {
-        vector<int> prime(n+1,1);
-        vector<int> low(n+1,1);
+    int func(int n){
+        if(n==1) return 0;
+        int ans=0;
         for(int i=2; i<=n; i++){
-            if(prime[i]){
-                for(int j=2*i; j<=n; j+=i){
-                    prime[j]=0;
-                    if(low[j]==1) low[j]=i;
-                }
+            if(n%i==0){
+                ans=func(n/i)+i;
+                break;
             }
         }
-        vector<int> dp(n+1,0);
-        for(int i=2; i<=n; i++){
-            if(low[i]==1) dp[i]=i;
-            else dp[i]=dp[i/low[i]]+low[i];
-        }
-        // for(int i=1; i<=n; i++) cout<<dp[i]<<" ";
-        return dp[n];
+        return ans;
+    }
+    int minSteps(int n) {
+        return func(n);
     }
 };
