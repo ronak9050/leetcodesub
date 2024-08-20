f3841f80d74a4cbd5c68df5ac599aef8c1d9e999
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-20 14:02:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (68.55%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -17,7 +17,7 @@ public:
     }
 
     bool stoneGame(vector<int>& a) {
-        return true;
+        return 1;
         int n=a.size();
         vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(2,-1)));
         int sum=accumulate(a.begin(),a.end(),0);
