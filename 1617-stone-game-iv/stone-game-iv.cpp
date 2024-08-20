f7f9692e656b6edc18b255da5d2a33f08f88acdb
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-20 14:40:59 +0530

    Time: 181 ms (17.50%) | Memory: 9.3 MB (53.91%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<int> dp(n+1,0);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sqrt(i); j++){
+                if(!dp[i-j*j]) dp[i]=1;
+            }
+        }
+        return dp[n];
+    }
+};
