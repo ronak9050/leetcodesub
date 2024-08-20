74d6654d14be157d79a23e6b2f9ee59b7a10c457
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-20 14:41:26 +0530

    Time: 40 ms (81.37%) | Memory: 9.4 MB (53.91%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -4,7 +4,10 @@ public:
         vector<int> dp(n+1,0);
         for(int i=1; i<=n; i++){
             for(int j=1; j<=sqrt(i); j++){
-                if(!dp[i-j*j]) dp[i]=1;
+                if(!dp[i-j*j]){
+                    dp[i]=1;
+                    break;
+                }
             }
         }
         return dp[n];
