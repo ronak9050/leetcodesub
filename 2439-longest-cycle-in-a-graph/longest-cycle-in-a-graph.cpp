5cb9d5b2a71a6f83ddd598e301a63cd39ac050d2
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-21 17:40:29 +0530

    Time: 113 ms (95.67%) | Memory: 93 MB (96.43%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -1,46 +1,23 @@
 class Solution {
-public:
-    vector<int> m;
-    vector<vector<int>> adj;
-    vector<int> dp;
-    int n;
+ public:
+  int longestCycle(vector<int>& edges) {
+    int ans = -1;
+    int time = 1;
+    vector<int> timeVisited(edges.size());
 
-    int dfs(int x, int k) {
-        if (dp[x] != -1)
-            return dp[x];
-        if (m[x] != -1)
-            return k - m[x];
-
-        m[x] = k;
-        int ans = 0;
-        for (auto& c : adj[x]) {
-            ans = max(ans, dfs(c, k + 1));
-        }
-        dp[x] = ans;
-        return ans;
+    for (int i = 0; i < edges.size(); ++i) {
+      if (timeVisited[i])
+        continue;
+      const int startTime = time;
+      int u = i;
+      while (u != -1 && !timeVisited[u]) {
+        timeVisited[u] = time++;
+        u = edges[u];  // Move to the next node.
+      }
+      if (u != -1 && timeVisited[u] >= startTime)
+        ans = max(ans, time - timeVisited[u]);
     }
 
-    int longestCycle(vector<int>& a) {
-        this->n = a.size();
-        adj.resize(n);
-        m.assign(n, -1);
-        dp.assign(n, -1);
-      
-
-        for (int i = 0; i < n; i++) {
-            if (a[i] != -1 && a[i] < n) {
-                adj[i].push_back(a[i]);
-            }
-        }
-
-        int ans = -1;
-        for (int i = 0; i < n; i++) {
-            if (dp[i] == -1) {
-                ans = max(ans, dfs(i, 0));
-            }
-        }
-        if(ans==0)
-         ans=-1;
-        return ans;
-    }
+    return ans;
+  }
 };
