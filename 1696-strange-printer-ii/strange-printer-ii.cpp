721041d1a70d5ac1a6cf410372063aef93410a38
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-22 00:39:21 +0530

    Time: 14 ms (97.44%) | Memory: 16.9 MB (91.03%) - LeetSync

--- 1696-strange-printer-ii/strange-printer-ii.cpp
+++ 1696-strange-printer-ii/strange-printer-ii.cpp
@@ -4,7 +4,6 @@ public:
         int n=a.size(),m=a[0].size();
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++) {
-                cout<<a[i][j]<<" ";
                 if(a[i][j]!=-1) return 0;
             }
             cout<<endl;
