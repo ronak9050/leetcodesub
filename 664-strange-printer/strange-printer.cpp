2b0b91e72bb82c1eff09e4e256622ab2693b07b6
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-21 14:56:25 +0530

    Time: 31 ms (43.16%) | Memory: 11.3 MB (37.94%) - LeetSync

--- 664-strange-printer/strange-printer.cpp
+++ 664-strange-printer/strange-printer.cpp
@@ -12,7 +12,16 @@ public:
 
     int strangePrinter(string s) {
         int n=s.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return func(0,n-1,s,dp)+1;
+        vector<vector<int>> dp(n,vector<int>(n,1e6));
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j<n; j++){
+                if(i==j) dp[i][j]=0;
+                for(int p=i; p<j; p++){
+                    dp[i][j]=min(dp[i][j],(s[i]!=s[j])+dp[i][p]+dp[p+1][j]);
+                }
+            }
+        }
+        return dp[0][n-1]+1;
     }
 };
