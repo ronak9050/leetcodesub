3399419b11f0eab113f8ff69df8dd94e24910f23
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-23 19:30:57 +0530

    Time: 151 ms (55.42%) | Memory: 88 MB (28.01%) - LeetSync

--- 1808-stone-game-vii/stone-game-vii.cpp
+++ 1808-stone-game-vii/stone-game-vii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
+    vector<vector<int>> dp ;
+     int f(vector<int> &v, int st, int end,int sum) {
+        if (st >= end || sum <= 0) return 0; // If there is only one stone, the score difference is 0.
+        if (dp[st][end] != -1) return dp[st][end];  
+        int front_score , end_score ;
 
-    int func(int i,int j,int t,vector<int>&a,vector<vector<vector<int>>>&dp){
-        if(i>=j) return 0;
-        if(dp[i][j][t]!=-1) return dp[i][j][t];
-        int ans;
-        if(t){
-            ans=max(a[j]-a[i]+func(i+1,j,1-t,a,dp),a[j-1]-(i>0? a[i-1]:0)+func(i,j-1,1-t,a,dp));
-        }
-        else{
-            ans=min(-a[j]+a[i]+func(i+1,j,1-t,a,dp),-a[j-1]+(i>0? a[i-1]:0)+func(i,j-1,1-t,a,dp));
-        }
-        return dp[i][j][t]=ans;
+        front_score = sum - v[st] - f(v , st+1 , end , sum-v[st]) ;
+        end_score = sum - v[end] - f(v , st , end-1 , sum-v[end]) ;
+        return dp[st][end] = max(front_score , end_score);
     }
-
-
-    int stoneGameVII(vector<int>& a) {
-        int n=a.size();
-        vector<int> hsh(n,0);
-        for(int i=0; i<n; i++){
-            if(i) hsh[i]=hsh[i-1];
-            hsh[i]+=a[i];
-        }
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
-        return func(0,n-1,1,hsh,dp);
+    int stoneGameVII(vector<int>& v) {
+        int n = v.size() , sum = 0 ;
+        for(auto a : v) sum += a ;
+        dp.clear() ;
+        dp.resize(n + 1, vector<int>(n + 1, -1));
+        return  f(v , 0 , n-1,sum)  ;
     }
 };
