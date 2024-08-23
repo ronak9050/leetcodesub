a38b3f552ec71a59aaa93666ed68b5d9785ddfff
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-23 17:23:08 +0530

    Time: 251 ms (19.23%) | Memory: 140.4 MB (21.85%) - LeetSync

--- 1788-stone-game-vi/stone-game-vi.cpp
+++ 1788-stone-game-vi/stone-game-vi.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    // Custom comparator to sort the stones based on the combined value for Alice and Bob
     static bool cmp(vector<int>& a, vector<int>& b) {
         return a[0] + a[1] > b[0] + b[1];
     }
 
     int stoneGameVI(vector<int>& a, vector<int>& b) {
         int n = a.size();
-        // Combine the values for Alice and Bob into a single vector
-        vector<vector<int>> combined_vector(n, vector<int>(2));
+        vector<vector<int>> pos(n);
         for (int i = 0; i < n; i++) {
-            combined_vector[i][0] = a[i];
-            combined_vector[i][1] = b[i];
+            pos[i]={a[i],b[i]};
         }
 
-        // Sort the combined vector based on the sum of values
-        sort(combined_vector.begin(), combined_vector.end(), cmp);
+        sort(pos.begin(), pos.end(), cmp);
 
-        int alice = 0, bob = 0;
-        // Alternate picking stones between Alice and Bob
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                // Alice's turn
-                alice += combined_vector[i][0];
-            } else {
-                // Bob's turn
-                bob += combined_vector[i][1];
-            }
+        int sum=0;
+        for (int i=0; i<n; i++) {
+            if (i&1) sum-=pos[i][1];
+            else sum+=pos[i][0];
         }
 
-        // Determine the winner
-        return alice > bob ? 1 : alice == bob ? 0 : -1;
+        if(sum) return (sum>0? 1:-1);
+        return 0;
     }
 };
