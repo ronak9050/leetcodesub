59bd6416bc8293f8baee72ff029cfef535fe725a
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-24 16:48:02 +0530

    Time: 121 ms (73.93%) | Memory: 16.8 MB (81.79%) - LeetSync

--- 1808-stone-game-vii/stone-game-vii.cpp
+++ 1808-stone-game-vii/stone-game-vii.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int dp[1001][1001] = {};
     int dfs(vector<int>& s, int i, int j, int sum) {
-        if (i == j)
-            return 0;
-        return dp[i][j] ? dp[i][j] : dp[i][j] = max(sum - s[i] - dfs(s, i + 1, j, sum - s[i]),
-            sum - s[j] - dfs(s, i, j - 1, sum - s[j]));
+        if (i == j) return 0;
+        if(dp[i][j]) return dp[i][j];
+        int ans = sum-s[i]-dfs(s,i+1,j,sum-s[i]);
+        ans=max(ans,sum-s[j]-dfs(s,i,j-1,sum-s[j]));
+        return dp[i][j]=ans;
     }
     int stoneGameVII(vector<int>& s) {
         return dfs(s, 0, s.size() - 1, accumulate(begin(s), end(s), 0));
