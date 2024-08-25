9cbdb23c6ccf857a205d166cbf8be62b31380260
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-25 14:00:37 +0530

    Time: 133 ms (86.79%) | Memory: 60.2 MB (57.95%) - LeetSync

--- 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.cpp
+++ 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.cpp
@@ -3,18 +3,13 @@ class Solution {
 public:
     long long maxSpending(vector<vector<int>>& a) {
         int n=a.size(),m=a[0].size();
-        priority_queue<vector<ll>,vector<vector<ll>>,greater<vector<ll>>> pq;
+        vector<int> ans(m*n);
         for(int i=0; i<n; i++){
-            pq.push({a[i][m-1],i,m-1});
+            for(int j=0; j<m; j++) ans[i*m+j]=a[i][j];
         }
-        ll ans=0,d=1;
-        while(pq.size()){
-            auto cur=pq.top();
-            pq.pop();
-            ans+=cur[0]*d;
-            d++;
-            if(cur[2]>0) pq.push({a[cur[1]][cur[2]-1],cur[1],cur[2]-1});
-        }
-        return ans;
+        sort(ans.begin(),ans.end());
+        ll ret=0;
+        for(ll i=0; i<m*n; i++) ret+=(i+1)*ans[i];
+        return ret;
     }
 };
