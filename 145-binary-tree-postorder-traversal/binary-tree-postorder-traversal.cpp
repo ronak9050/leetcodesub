5b513062e56169501ad91a204f3c2127198f3ee8
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-25 12:28:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (7.15%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -13,27 +13,27 @@ class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> pre,post,ino;
-    stack<pair<TreeNode*,int>> st;
-        if(root==nullptr) return post;
-    st.push({root,1});
-    while(!st.empty()){
-        int num=st.top().second;
-        TreeNode* temp=st.top().first;
-        if(num==1){
-            pre.push_back(temp->val);
-            st.top().second++;
-            if(temp->left!=nullptr) st.push({temp->left,1}); 
+        stack<pair<TreeNode*,int>> st;
+            if(root==nullptr) return post;
+        st.push({root,1});
+        while(!st.empty()){
+            int num=st.top().second;
+            TreeNode* temp=st.top().first;
+            if(num==1){
+                pre.push_back(temp->val);
+                st.top().second++;
+                if(temp->left!=nullptr) st.push({temp->left,1}); 
+            }
+            else if(num==2){
+                ino.push_back(temp->val);
+                st.top().second++;
+                if(temp->right!=nullptr) st.push({temp->right,1});
+            }
+            else{
+                post.push_back(temp->val);
+                st.pop();
+            }
         }
-        else if(num==2){
-            ino.push_back(temp->val);
-            st.top().second++;
-            if(temp->right!=nullptr) st.push({temp->right,1});
-        }
-        else{
-            post.push_back(temp->val);
-            st.pop();
-        }
-    }
         return post;
     }
 };
