6eaa949d2b6005207b666a289d64b2c1a09b90bd
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-25 13:12:38 +0530

    Time: 140 ms (32.57%) | Memory: 571.2 MB (22.67%) - LeetSync

--- 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int func(int ind,int i,int j,vector<int>&a,vector<int> &mul,map<vector<int>,int>&mp){
-        int m=mul.size();
+    int func(int ind,int i,vector<int>&a,vector<int> &mul,vector<vector<int>>&dp){
+        int m=mul.size(),n=a.size();
         if(ind>=m) return 0;
-        if(mp.find({ind,i,j})!=mp.end()) return mp[{ind,i,j}];
-        int ans=max(mul[ind]*a[j]+func(ind+1,i,j-1,a,mul,mp),mul[ind]*a[i]+func(ind+1,i+1,j,a,mul,mp));
-        return mp[{ind,i,j}]=ans;
+        if(dp[ind][i]!=-1) return dp[ind][i];
+
+        int ans=max(mul[ind]*a[n-1-(ind-i)]+func(ind+1,i,a,mul,dp),mul[ind]*a[i]+func(ind+1,i+1,a,mul,dp));
+
+        return dp[ind][i]=ans;
     }
     int maximumScore(vector<int>& a, vector<int>& mul) {
-        map<vector<int>,int> mp;
         int n=a.size();
-        return func(0,0,n-1,a,mul,mp);
+        vector<vector<int>> dp(mul.size(),vector<int>(n,-1));
+        return func(0,0,a,mul,dp);
     }
 };
