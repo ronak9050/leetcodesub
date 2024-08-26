4f6b5b7312b60718a14b660e444abc0c01a07664
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-26 18:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (20.31%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int func(int i,int j,int t,vector<int>&a,vector<vector<vector<int>>>&dp){
+    int func(int i,int j,vector<int>&a,vector<vector<int>>&dp){
         if(i>j) return 0;
-        if(dp[i][j][t]!=-1) return dp[i][j][t];
-        int ans;
-        if(t){
-            ans=max(a[i]+func(i+1,j,1-t,a,dp),a[j]+func(i,j-1,1-t,a,dp));
-        }
-        else{
-            ans=min(-a[i]+func(i+1,j,1-t,a,dp),-a[j]+func(i,j-1,1-t,a,dp));
-        }
-        return dp[i][j][t]=ans;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=max(a[i]-func(i+1,j,a,dp),a[j]-func(i,j-1,a,dp));
+        return dp[i][j]=ans;
     }
     bool predictTheWinner(vector<int>& a) {
         int n=a.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(2,-1)));
-        int ans=func(0,n-1,1,a,dp);
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        int ans=func(0,n-1,a,dp);
         if(ans>=0) return 1;
         return 0;
     }
