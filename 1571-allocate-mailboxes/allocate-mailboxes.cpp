5737f84ecf1c368545e2a6f0917abbfbc7aa93e8
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-26 17:22:53 +0530

    Time: 14 ms (62.17%) | Memory: 10.7 MB (44.82%) - LeetSync

--- 1571-allocate-mailboxes/allocate-mailboxes.cpp
+++ 1571-allocate-mailboxes/allocate-mailboxes.cpp
@@ -1,30 +1,27 @@
 #define ll long long
 class Solution {
 public:
-    ll func(int i,int j,int k,vector<int>&a,vector<vector<vector<ll>>>&dp){
-        if(i>j) return 0;
-        if(k==0) return INT_MAX;
-        if(dp[i][j][k]!=-1) return dp[i][j][k];
-        ll ans=INT_MAX,sum=0,ct=0;
-
-        for(int p=i; p<=j; p++){
-
-            sum+=a[p];
-            ct++;
-            ll cur=0;
-            for(int q=i; q<i+ct; q++) {
-                cur+=abs(a[q]-a[i+ct/2]);
-            }
-
-            ans=min(ans,cur+func(p+1,j,k-1,a,dp));
+    int dp[101][101] = {};
+    int dfs(vector<int>& h, vector<vector<int>> &costs, int i, int k) {
+        if (i >= h.size() || k <= 0)
+            return i == h.size() && k == 0 ? 0 : 10000000;
+        if (!dp[i][k]) {
+            dp[i][k] = 10000000;
+            for (auto j = i; j <= h.size() - k; ++j)
+                dp[i][k] = min(dp[i][k], costs[i][j] + dfs(h, costs, j + 1, k - 1));
         }
-        return dp[i][j][k]=ans;
+        return dp[i][k];
     }
-
-    int minDistance(vector<int>& a, int k) {
-        sort(a.begin(),a.end());
-        int n=a.size();
-        vector<vector<vector<ll>>> dp(n,vector<vector<ll>>(n,vector<ll>(k+1,-1)));
-        return func(0,n-1,k,a,dp);
+    int minDistance(vector<int>& h, int k) {      
+        sort(begin(h), end(h));
+        vector<vector<int>> costs(h.size(), vector<int>(h.size()));
+        for (auto i = 0; i < h.size(); ++i) {
+            for (auto j = i; j < h.size(); ++j) {
+                int mailbox = h[(i + j) / 2];
+                for (auto p = i; p <= j; ++p)
+                    costs[i][j] += abs(mailbox - h[p]);
+            }
+        }
+        return dfs(h, costs, 0, k);
     }
 };
