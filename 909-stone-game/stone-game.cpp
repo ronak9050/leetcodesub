ad5c86260ae5a9198fecc70baa836e4c43f95097
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-26 19:20:10 +0530

    Time: 19 ms (15.02%) | Memory: 18.6 MB (11.29%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int func(int i,int j,int k,vector<int>&a,vector<vector<vector<int>>>&dp){
+    int func(int i,int j,vector<int>&a,vector<vector<int>>&dp){
         if(i>j) return 0;
-        if(dp[i][j][k]!=-1) return dp[i][j][k];
-        int ans=0;
-        if(k){
-            ans=max(ans,a[i]+func(i+1,j,1-k,a,dp));
-            ans=max(ans,a[j]+func(i,j-1,1-k,a,dp));
-        }
-        else{
-            ans=INT_MAX;
-            ans=min(ans,a[i]+func(i+1,j,1-k,a,dp));
-            ans=min(ans,a[j]+func(i,j-1,1-k,a,dp));
-        }
-        return dp[i][j][k]=ans;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=max(a[i]-func(i+1,j,a,dp),a[j]-func(i,j-1,a,dp));
+        return dp[i][j]=ans;
     }
 
     bool stoneGame(vector<int>& a) {
-        return 1;
+        // return 1;
         int n=a.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(2,-1)));
-        int sum=accumulate(a.begin(),a.end(),0);
-        int alc=func(0,n-1,1,a,dp);
-        return (alc>sum-alc);
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return func(0,n-1,a,dp)>0;
     }
 };
