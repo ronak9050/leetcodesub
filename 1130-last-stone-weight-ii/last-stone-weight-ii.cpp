7cfab234a977392557bf625216946132fe00a279
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-27 19:15:33 +0530

    Time: 8 ms (28.20%) | Memory: 14.1 MB (22.61%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int func(int i,int sum,vector<int>&a,map<pair<int,int>,int> &mp){
+    int func(int i,int cur,int sum,vector<int>&a,vector<vector<int>> &dp){
         int n=a.size();
-        if(i>=n) return abs(sum);
-        if(mp.find({i,sum})!=mp.end()) return mp[{i,sum}];
-        int ans=min(func(i+1,sum-a[i],a,mp),func(i+1,sum+a[i],a,mp));
-        return mp[{i,sum}]=ans;
+        if(i>=n) return abs(cur);
+        if(dp[i][sum+cur]!=-1) return dp[i][sum+cur];
+        int ans=min(func(i+1,cur-a[i],sum,a,dp),func(i+1,cur+a[i],sum,a,dp));
+        return dp[i][sum+cur]=ans;
     }
 
     int lastStoneWeightII(vector<int>& a) {
         int n=a.size();
-        map<pair<int,int>,int> mp;
-        return func(0,0,a,mp);
+        int sum=accumulate(a.begin(),a.end(),0);
+        vector<vector<int>> dp(n,vector<int>(2*sum+3,-1));
+        return func(0,0,sum,a,dp);
     }
 };
