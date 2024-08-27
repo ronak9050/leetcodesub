59f1eb3ff5cd07e47ef928a7370b521827d3d76d
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-28 00:00:37 +0530

    Time: 95 ms (74.32%) | Memory: 25 MB (14.19%) - LeetSync

--- 1024-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.cpp
+++ 1024-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.cpp
@@ -4,18 +4,19 @@ public:
         int n=a.size(),ans=0;
         vector<bitset<1000>> hsh(17);
         vector<int> v(n*n);
+        unordered_map<int,int> mp;
         for(int i=0; i<n; i++){
             for(int j=0; j<17; j++) if((a[i]>>j)&1) hsh[j][i]=1;
-            for(int j=0; j<n; j++) v[i*n+j]=(a[i]&a[j]);
+            for(int j=0; j<n; j++) mp[(a[i]&a[j])]++;
         }
 
-        for(auto &e:v){
+        for(auto &e:mp){
             bitset<1000> cur;
             for(int j=0; j<17; j++){
-                if((e>>j)&1) cur=(cur|hsh[j]);
+                if((e.first>>j)&1) cur=(cur|hsh[j]);
             }
             int num=(n-cur.count());
-            ans+=num;
+            ans+=num*e.second;
         }
 
         return ans;
