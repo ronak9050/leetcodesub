21a1d853e8a1efea3e2bcbb039684f1cb4e831e0
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-08-28 13:04:50 +0530

    Time: 221 ms (52.97%) | Memory: 107.6 MB (35.17%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> moves={{1,0},{-1,0},{0,1},{0,-1}};
-    void dfs(int i,int j,vector<vector<int>>&a,vector<vector<int>>&vis){
+    int dfs(int i,int j,vector<vector<int>>&a,vector<vector<int>>&b,vector<vector<int>>&vis){
         vis[i][j]=1;
-        int n=a.size(),m=a[0].size();
+        int n=a.size(),m=a[0].size(),ans=1;
+        if(!a[i][j]) ans=0;
         for(auto &e:moves){
             int x=i+e[0],y=j+e[1];
-            if(x>=0 && y>=0 && x<n && y<m && !vis[x][y] && a[x][y]) dfs(x,y,a,vis);
-        }
-    }
-
-    int dfs2(int i,int j,vector<vector<int>>&b,vector<vector<int>>&vis,vector<vector<int>>&vis2){
-        vis2[i][j]=1;
-        int ans=1,n=b.size(),m=b[0].size();
-        if(!vis[i][j]) {
-            // cout<<i<<" "<<j<<endl;
-            ans=0;
-        }
-        for(auto &e:moves){
-            int x=i+e[0],y=j+e[1];
-            if(x>=0 && y>=0 && x<n && y<m && !vis2[x][y] && b[x][y]) ans&=dfs2(x,y,b,vis,vis2);
+            if(x>=0 && y>=0 && x<n && y<m && !vis[x][y] && b[x][y]) ans&=dfs(x,y,a,b,vis);
         }
         return ans;
     }
@@ -31,14 +19,8 @@ public:
         int ans=0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(!vis[i][j] && a[i][j]) dfs(i,j,a,vis);
-            }
-        }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(!vis2[i][j] && b[i][j]) {
-                    int cur=dfs2(i,j,b,vis,vis2);
+                if(!vis[i][j] && b[i][j]) {
+                    int cur=dfs(i,j,a,b,vis);
                     // cout<<i<<" "<<j<<" "<<cur<<endl;
                     ans+=cur;
                 }
