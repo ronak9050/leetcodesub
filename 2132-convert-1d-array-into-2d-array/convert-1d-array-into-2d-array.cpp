0fcd1e99accff3e5daad720570bb708fd1b5cb73
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-09-01 11:41:53 +0530

    Time: 74 ms (83.60%) | Memory: 88.1 MB (73.42%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        int sz=original.size();
+        if(m*n!=sz) return {};
+        vector<vector<int>> ans(m,vector<int>(n));
+        for(int i=0; i<sz; i++){
+            ans[i/n][i%n]=original[i];
+        }
+        return ans;
+    }
+};
