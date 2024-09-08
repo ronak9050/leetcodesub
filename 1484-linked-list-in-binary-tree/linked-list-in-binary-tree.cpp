2351367084e23cce8160a00138040aee045231fc
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-09-08 14:54:13 +0530

    Time: 23 ms (70.36%) | Memory: 30.7 MB (88.80%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
@@ -1,45 +1,39 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool func(TreeNode* r,ListNode* cur,ListNode* h,map<pair<TreeNode*,ListNode*>,bool>&mp){
-        if(cur==nullptr) return 1;
-        if(r==nullptr) return 0;
-        if(mp.find({r,cur})!=mp.end()) return mp[{r,cur}];
-        
-        if(func(r->left,h,h,mp)) return mp[{r,cur}]=1;
-        if(func(r->right,h,h,mp)) return mp[{r,cur}]=1;
-        if(r->val==h->val){
-            if(func(r->left,h->next,h,mp)) return mp[{r,cur}]=1;
-            if(func(r->right,h->next,h,mp)) return mp[{r,cur}]=1;
-        }
-        if(r->val==cur->val && cur!=h){
-            if(func(r->left,cur->next,h,mp)) return mp[{r,cur}]=1;
-            if(func(r->right,cur->next,h,mp)) return mp[{r,cur}]=1;
-        }
-        return mp[{r,cur}]=0;
-    }
     bool isSubPath(ListNode* head, TreeNode* root) {
-        map<pair<TreeNode*,ListNode*>,bool> mp;
-        return func(root,head,head,mp);
+        // Build the pattern and prefix table from the linked list
+        vector<int> pattern = {head->val}, prefixTable = {0};
+        int patternIndex = 0;
+        head = head->next;
+
+        while (head) {
+            while (patternIndex && head->val != pattern[patternIndex])
+                patternIndex = prefixTable[patternIndex - 1];
+            patternIndex += head->val == pattern[patternIndex];
+            pattern.push_back(head->val);
+            prefixTable.push_back(patternIndex);
+            head = head->next;
+        }
+
+        // Perform DFS to search for the pattern in the tree
+        return searchInTree(root, 0, pattern, prefixTable);
+    }
+
+private:
+    bool searchInTree(TreeNode* node, int patternIndex,
+                      const vector<int>& pattern,
+                      const vector<int>& prefixTable) {
+        if (!node) return false;
+
+        while (patternIndex && node->val != pattern[patternIndex])
+            patternIndex = prefixTable[patternIndex - 1];
+        patternIndex += node->val == pattern[patternIndex];
+
+        // Check if the pattern is fully matched
+        if (patternIndex == pattern.size()) return true;
+
+        // Recursively search left and right subtrees
+        return searchInTree(node->left, patternIndex, pattern, prefixTable) ||
+               searchInTree(node->right, patternIndex, pattern, prefixTable);
     }
 };
