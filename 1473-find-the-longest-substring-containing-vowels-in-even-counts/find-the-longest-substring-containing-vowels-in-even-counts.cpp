b7ef7df9e1421687d3bc3b1d443c8ec10d157a65
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-09-15 14:12:47 +0530

    Time: 43 ms (71.16%) | Memory: 17.5 MB (85.12%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        int n=s.size();
-        unordered_map<int,vector<int>> mp;
-        int cur=0;
-        vector<int> pos(150,0);
+        int n=s.size(),cur=0,ans=0;
+        vector<int> mp(65,-1),pos(150,0);
         pos['a']=1;pos['e']=2;pos['i']=3;pos['o']=4;pos['u']=5;
+
         for(int i=0; i<n; i++){
             if(pos[s[i]]){
                 cur=cur^(1<<(pos[s[i]]));
             }
-            mp[cur].push_back(i);
+            if(mp[cur]==-1) mp[cur]=i;
+            else ans=max(ans,i-mp[cur]);
+            if(cur==0) ans=max(ans,i+1);
         }
-        
-        int ans=0;
-        for(auto &e:mp){
-            if(e.second.size()>1){
-                ans=max(ans,e.second.back()-e.second[0]);
-            }
-        }
-        if(mp[0].size()!=0) ans=max(ans,mp[0].back()+1);
+
         return ans;
     }
 };
