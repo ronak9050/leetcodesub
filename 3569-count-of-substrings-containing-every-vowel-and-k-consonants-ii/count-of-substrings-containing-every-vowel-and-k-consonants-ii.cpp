b9bc8e18a2dbb1468ed829fce50c26c5fb89af0f
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-09-30 04:15:57 +0530

    Time: 500 ms (63.64%) | Memory: 144.2 MB (54.55%) - LeetSync

---
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -0,0 +1,67 @@
+#define ll long long
+
+class Solution {
+public:
+    vector<int> mp;
+    long long countOfSubstrings(string s, int req) {
+        mp.resize(26,0);
+        mp[0]=1;mp[4]=2;mp[8]=3;mp[14]=4;mp[20]=5;
+        ll n=s.size(),ans=0,ct=0,last=-1;
+
+        vector<int> cmin(n,-1),cmax(n,-1),pos,vmax(n,-1);
+        vector<vector<int>> prevpos(6);
+        
+
+        if(req){
+            for(int i=0; i<n; i++){
+                if(mp[s[i]-'a']){
+                    prevpos[mp[s[i]-'a']].push_back(i);
+                }
+
+                int cur=n;
+                for(int j=1; j<6; j++){
+                    if(prevpos[j].size()) cur=min(cur,prevpos[j].back());
+                    else cur=-1;
+                }
+                vmax[i]=cur;
+            }
+            for(int i=0; i<n; i++){
+                if(!mp[s[i]-'a']){
+                    pos.push_back(i);
+                    if(pos.size()>=req) cmax[i]=pos[pos.size()-req];
+                    if(pos.size()>req) cmin[i]=pos[pos.size()-req-1];     
+                }
+                else{
+                    if(i) cmax[i]=cmax[i-1];
+                    if(i) cmin[i]=cmin[i-1];
+                }
+            }
+            for(int i=0; i<n; i++){
+                
+                if(min(vmax[i],cmax[i])>cmin[i]) ans=ans+min(vmax[i],cmax[i])-cmin[i];
+            }
+            return ans;
+        }
+        else{
+            for(int i=0; i<n; i++){
+                if(mp[s[i]-'a']){
+                    prevpos[mp[s[i]-'a']].push_back(i);
+                }
+                else last=i;
+                cmin[i]=last;
+
+                int cur=n;
+                for(int j=1; j<6; j++){
+                    if(prevpos[j].size()) cur=min(cur,prevpos[j].back());
+                    else cur=-1;
+                }
+                vmax[i]=cur;
+            }
+            for(int i=0; i<n; i++) {
+                cout<<cmin[i]<<" "<<vmax[i]<<endl;
+                if(cmin[i]<vmax[i]) ans=ans+vmax[i]-cmin[i];
+            }
+        }
+        return ans;
+    }
+};
