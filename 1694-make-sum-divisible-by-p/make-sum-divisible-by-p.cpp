b8967cc0b643465fa369b7beb8383b3bb13e4e0b
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-10-03 11:20:02 +0530

    Time: 222 ms (14.14%) | Memory: 73.4 MB (18.77%) - LeetSync

---
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -0,0 +1,21 @@
+#define ll long long
+
+class Solution {
+public:
+    int minSubarray(vector<int>& a, int p) {
+        int n=a.size();
+        ll sum=accumulate(a.begin(),a.end(),0ll);
+        if(sum<p) return -1;
+        ll ans=sum%p? n:0;
+        map<ll,ll> mp;
+        mp[0]=-1;
+        ll cur=0;
+        for(int i=0; i<n; i++){
+            cur=cur+a[i];
+            ll req=(cur%p-sum%p+p)%p;
+            if(mp.find(req)!=mp.end()) ans=min(ans,i-mp[req]);
+            mp[cur%p]=i;
+        }
+        return ans==n? -1:ans;
+    }
+};
