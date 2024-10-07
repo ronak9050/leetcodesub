2640c2cb6b704ffe077b41d442650934c8c6c8e2
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-10-07 13:37:43 +0530

    Time: 8 ms (42.58%) | Memory: 11.6 MB (31.24%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
     int minLength(string s) {
-        int rm[105]={0};
-        int n=s.length();
-        
-        for(int k=0; k<=n/2; k++){
-            for(int i=0; i<n-1; i++){
-                while(rm[i]){
-                    i++;
-                }
-                int j=i+1;
-                while(rm[j]){
-                    j++;
-                }
-                if(i<n && j<n && s[i]=='A' && s[j]=='B'){
-                    rm[i]=1;
-                    rm[j]=1;
-                    i=j+1;
-                }
-                else if(i<n && j<n && s[i]=='C' && s[j]=='D'){
-                    rm[i]=1;
-                    rm[j]=1;
-                    i=j+1;
-                }
+        stack<char> st;
+        for(int i=0; i<s.size(); i++){
+            if(st.size() && ((st.top()=='A' && s[i]=='B') || (st.top()=='C' && s[i]=='D'))){
+                st.pop();
             }
+            else st.push(s[i]);
         }
-        int ans=0;
-        for(int i=0; i<n; i++){
-            if(rm[i]==0) ans++;
-        }
-        return ans;
+        return st.size();
     }
 };
