df76d6c807a9fbccb7247a207825165c37935411
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-10-25 16:38:47 +0530

    Time: 770 ms (17.88%) | Memory: 139.1 MB (16.73%) - LeetSync

--- 464-can-i-win/can-i-win.cpp
+++ 464-can-i-win/can-i-win.cpp
@@ -3,12 +3,12 @@ public:
     int func(int turn,int mask,int tot,int mx,vector<vector<int>>&dp){
         int sum=0;
         if(dp[turn][mask]!=-1) return dp[turn][mask];
-        for(int i=1; i<=mx; i++){
-            if((mask>>i)&1) sum+=i;
+        for(int i=0; i<mx; i++){
+            if((mask>>i)&1) sum+=i+1;
         }
         if(tot<=sum) return !turn;
         int ans=1-turn;
-        for(int i=1; i<=mx; i++){
+        for(int i=0; i<mx; i++){
             if((mask&(1<<i))==0){
                 if(turn) ans|=func(1-turn,mask|(1<<i),tot,mx,dp);
                 else ans&=func(1-turn,mask|(1<<i),tot,mx,dp);
@@ -19,7 +19,7 @@ public:
     bool canIWin(int mx, int tot) {
         if(mx*(mx+1)/2<tot) return 0;
         if(tot==0) return 1;
-        vector<vector<int>> dp(2,vector<int>((1<<(mx+1)),-1));
+        vector<vector<int>> dp(2,vector<int>((1<<(mx)),-1));
         return func(1,0,tot,mx,dp);
     }
 };
