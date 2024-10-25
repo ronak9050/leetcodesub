d490a0e1f337df4ad99476443a4d88aef3f2ccd8
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-10-25 16:49:36 +0530

    Time: 411 ms (43.27%) | Memory: 51.4 MB (60.77%) - LeetSync

--- 464-can-i-win/can-i-win.cpp
+++ 464-can-i-win/can-i-win.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int func(int turn,int mask,int tot,int mx,vector<vector<int>>&dp){
-        int sum=0;
-        if(dp[turn][mask]!=-1) return dp[turn][mask];
-        for(int i=0; i<mx; i++){
-            if((mask>>i)&1) sum+=i+1;
-        }
-        if(tot<=sum) return !turn;
-        int ans=1-turn;
+    int func(int mask,int tot,int mx,vector<int>&dp){
+        if(dp[mask]!=-1) return dp[mask];
+        if(tot<=0) return 1;
+
+        int ans=0;
         for(int i=0; i<mx; i++){
             if((mask&(1<<i))==0){
-                if(turn) ans|=func(1-turn,mask|(1<<i),tot,mx,dp);
-                else ans&=func(1-turn,mask|(1<<i),tot,mx,dp);
+                ans|=func(mask|(1<<i),tot-(i+1),mx,dp);
             }
         }
-        return dp[turn][mask]=ans;
+        return dp[mask]=!ans;
     }
+
     bool canIWin(int mx, int tot) {
         if(mx*(mx+1)/2<tot) return 0;
         if(tot==0) return 1;
-        vector<vector<int>> dp(2,vector<int>((1<<(mx)),-1));
-        return func(1,0,tot,mx,dp);
+        vector<int> dp((1<<(mx)),-1);
+        return !func(0,tot,mx,dp);
     }
 };
