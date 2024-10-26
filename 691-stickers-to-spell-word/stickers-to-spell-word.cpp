48160579a37ce78ebcb1b92fb9ec7e515b9ac00d
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-10-26 15:18:25 +0530

    Time: 552 ms (29.99%) | Memory: 275 MB (31.66%) - LeetSync

---
+++ 691-stickers-to-spell-word/stickers-to-spell-word.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int dp[50][1<<15];
+
+    void precom(string &s,vector<int>&ct){
+        for(auto e:s) ct[e-'a']++;
+    }
+
+    int func(int ind,int mask,string &t,vector<string> &s){
+        int n=t.size(),m=s.size();
+        if(ind>=m){
+            if(mask==(1<<n)-1) return 0;
+            return 100;
+        }
+        if(dp[ind][mask]!=-1) return dp[ind][mask];
+        int ans=func(ind+1,mask,t,s);
+        vector<int> ct(26,0);
+        precom(s[ind],ct);
+        int f=0,cp=mask;
+        for(int i=0; i<n; i++){
+            if(ct[t[i]-'a'] && !(cp&(1<<i))){
+                cp|=(1<<i);
+                ct[t[i]-'a']--;
+                f=1;
+            }
+        }
+
+        if(f) ans=min(ans,1+func(ind,cp,t,s));
+
+        return dp[ind][mask]=ans;
+    }
+
+    int minStickers(vector<string>& s, string t) {
+        vector<int> ct(26,0);
+        for(auto &e:s){
+            for(auto f:e) ct[f-'a']=1;
+        }
+        for(auto e:t) if(!ct[e-'a']) return -1;
+        memset(dp,-1,sizeof(dp));
+        return func(0,0,t,s);
+    }
+};
