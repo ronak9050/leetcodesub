1d3f6f17a1a3b494425068876542b5a299676d95
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-10-27 19:12:39 +0530

    Time: 199 ms (6.17%) | Memory: 29.2 MB (27.15%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -3,16 +3,16 @@ public:
     int countSquares(vector<vector<int>>& a) {
         int ans=0;
         int n=a.size(),m=a[0].size();
-        for(int sz=1; sz<=min(n,m); sz++){
-            vector<vector<int>> cur=a;
-            for(int i=0; i<n; i++){
-                for(int j=0; j<m; j++){
-                    if(i) cur[i][j]+=cur[i-1][j];
-                    if(j) cur[i][j]+=cur[i][j-1];
-                    if(i&&j) cur[i][j]-=cur[i-1][j-1];
-                }
+        vector<vector<int>> cur=a;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i) cur[i][j]+=cur[i-1][j];
+                if(j) cur[i][j]+=cur[i][j-1];
+                if(i&&j) cur[i][j]-=cur[i-1][j-1];
             }
-
+        }
+        
+        for(int sz=1; sz<=min(n,m); sz++){
             for(int i=sz-1; i<n; i++){
                 for(int j=sz-1; j<m; j++){
                     int ct=cur[i][j];
