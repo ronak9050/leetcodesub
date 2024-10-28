11da65052924fd3f74d164020598b30263b8d09e
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-10-28 14:29:30 +0530

    Time: 74 ms (71.11%) | Memory: 169.5 MB (6.43%) - LeetSync

---
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mx=1;
+    int func(long cur,vector<int>&ct,vector<int> &ans){
+        if(cur>1e5 || !ct[cur]) return 0;
+        if(ans[cur]!=-1) return ans[cur];
+        ans[cur]=1+func(cur*1ll*cur,ct,ans);
+        mx=max(mx,ans[cur]);
+        return ans[cur];
+    }
+
+    int longestSquareStreak(vector<int>& a) {
+        sort(a.begin(),a.end());
+        vector<int> ct(1e5+1,0),ans(1e5+1,-1);
+        for(auto e:a) ct[e]++;
+        for(int i=2; i<=1e5; i++){
+            if(ct[i]) func(i,ct,ans);
+        }
+        return mx>1? mx:-1;
+    }
+};
