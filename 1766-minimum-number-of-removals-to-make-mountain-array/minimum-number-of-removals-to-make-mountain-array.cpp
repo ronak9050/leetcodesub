91b824fd414384a57629939473bcfa647f216bca
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-10-30 19:51:14 +0530

    Time: 3 ms (90.91%) | Memory: 15.2 MB (24.81%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     void lis(vector<int>&a,vector<int>&dp){
         int n=a.size();
-        for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(a[i]>a[j]) dp[i]=max(dp[i],1+dp[j]);
+        vector<int> v;
+        for(int i=0; i<n; i++){
+            auto it=lower_bound(v.begin(),v.end(),a[i]);
+            if(it==v.end()) v.push_back(a[i]);
+            else{
+                v[it-v.begin()]=a[i];
             }
+            dp[i]=v.size()-1;
         }
     }
 
