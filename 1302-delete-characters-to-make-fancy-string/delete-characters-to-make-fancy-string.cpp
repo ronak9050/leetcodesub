010c9cb7d575f8d4fa6fb0ab284fee147ddafcc2
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-11-01 14:27:37 +0530

    Time: 26 ms (58.44%) | Memory: 39.6 MB (88.37%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     string makeFancyString(string s) {
         string ans;
+        ans.reserve(s.size());
         for(auto e:s){
             int n=ans.size();
             if(ans.size()>1 && ans[n-1]==e && ans[n-2]==e) continue;
