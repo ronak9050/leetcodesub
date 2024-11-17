b1e9648d61df353ded25c9ca38d3dda955099094
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-11-17 16:08:09 +0530

    Time: 3 ms (64.21%) | Memory: 95.4 MB (88.66%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& a, int m) {
         int xr=0,n=a.size();
-        for(int i=0; i<n; i++) xr=xr^a[i];
-        vector<int> ans;
-        for(int i=n-1; i>=0; i--){
-            int cur=0;
-            for(int j=0; j<m; j++){
-                if(((xr>>j)&1)==0) cur|=(1<<j);
-            }
-            ans.push_back(cur);
-            xr=xr^a[i];
+        vector<int> ans(n);
+        for(int i=0; i<n; i++){
+            xr^=a[i];
+            ans[i] = (((1<<m)-1)&xr)^((1<<m)-1);
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
