95ba30788c3a5af452bf41d0def25a8128f5fb92
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-11-26 02:51:10 +0530

    Time: 27 ms (27.88%) | Memory: 16.5 MB (20.23%) - LeetSync

---
+++ 787-sliding-puzzle/sliding-puzzle.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    int slidingPuzzle(vector<vector<int>>& a) {
+        queue<pair<int,vector<vector<int>>>> q;
+        q.push({0,a});
+        map<vector<vector<int>>,bool> mp;
+        mp[a]=1;
+        vector<vector<int>> end={{1,2,3},{4,5,0}};
+        vector<vector<int>> moves={{0,1},{0,-1},{1,0},{-1,0}};
+        while(q.size()){
+            auto it=q.front();
+            q.pop();
+            int mov=it.first;
+            auto cur=it.second;
+            if(cur==end) return mov;
+
+            int indi,indj;
+            for(int i=0; i<2; i++){
+                for(int j=0; j<3; j++) if(cur[i][j]==0) indi=i,indj=j;
+            }
+
+            for(auto &e:moves){
+                int x=indi+e[0],y=indj+e[1];
+                if(x>=0 && y>=0 && x<2 && y<3){
+                    swap(cur[x][y],cur[indi][indj]);
+                    if(cur==end) return mov+1;
+                    if(!mp[cur]){
+                        q.push({mov+1,cur});
+                        mp[cur]=1;
+                    }
+                    swap(cur[x][y],cur[indi][indj]);
+                }
+            }
+
+        }
+        return -1;
+    }
+};
