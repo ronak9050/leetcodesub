8c1e9779a8a8b3c33e63ecec9542c23413fda439
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-11-30 12:48:44 +0530

    Time: 3 ms (65.09%) | Memory: 23.8 MB (5.05%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,51 +1,37 @@
 class MinStack {
-    stack<int> st1,st2,st3;
+    stack<int> primary;  // Primary stack to store all elements
+    stack<int> minStack; // Stack to store the minimum elements
 public:
     MinStack() {
-        
+        // Fast I/O setup (can be omitted if not using input/output in competitive programming)
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
     }
     
     void push(int val) {
-        st1.push(val);
-        while(st2.size() && st2.top()<val){
-            st3.push(st2.top());
-            st2.pop();
-        }
-        st2.push(val);
-        while(st3.size()){
-            st2.push(st3.top());
-            st3.pop();
+        primary.push(val);  // Push value onto the primary stack
+        // Push onto minStack if it's empty or the new value is <= current min
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
         }
     }
     
     void pop() {
-        int ans=st1.top();
-        st1.pop();
-        while(st2.top()!=ans){
-            st3.push(st2.top());
-            st2.pop();
-        }
-        st2.pop();
-        while(st3.size()){
-            st2.push(st3.top());
-            st3.pop();
+        if (primary.empty()) return;  // No operation if primary stack is empty
+        int topValue = primary.top(); // Get the top value
+        primary.pop();                // Pop from primary stack
+        // If the top value is the same as the top of minStack, pop from minStack
+        if (!minStack.empty() && topValue == minStack.top()) {
+            minStack.pop();
         }
     }
     
     int top() {
-        return st1.top();
+        return primary.top(); // Return the top value of the primary stack
     }
     
     int getMin() {
-        return st2.top();
+        return minStack.top(); // Return the top value of the minStack (current minimum)
     }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
