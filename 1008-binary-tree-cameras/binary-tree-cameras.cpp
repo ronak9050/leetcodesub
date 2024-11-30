c7c3948cf0f351ddd487f69e9ba9cde5bebf807c
ronak9050 <130243279+ronak9050@users.noreply.github.com>
2024-11-30 10:37:55 +0530

    Time: 11 ms (14.88%) | Memory: 23.8 MB (20.88%) - LeetSync

---
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ct=0;
+    map<TreeNode*,bool> mp;
+    void func(TreeNode* r, TreeNode* par){
+        if(r==nullptr || (r->left==nullptr && r->right==nullptr) ) return;
+        func(r->left,r);
+        func(r->right,r);
+        if((r->left && mp[r->left]==0) || (r->right && mp[r->right]==0)){
+            ct++;
+            mp[r]=1;
+            if(par) mp[par]=1;
+        }
+    }
+    int minCameraCover(TreeNode* root) {
+        func(root,nullptr);
+        if(!mp[root]) ct++;
+        return ct;
+    }
+};
